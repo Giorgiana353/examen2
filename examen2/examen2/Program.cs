b1af567764ad42da5d979efdcdc374be54efc5c9
Giorgiana353 <giorgiana.dinu@emanuel.ro>
2024-01-22 10:16:50 +0200

    incercare

---
+++ examen2/examen2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Data;
+
+namespace examen2
+{
+public class Payments
+    {
+        public string? Customers { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Total { get; set; }
+    }
+    public Payments(string customers, DateTime date, decimal total)
+    {
+        string Customers = customers;
+        DateTime Date =  date;
+        decimal Total = total;
+    }
+    class Program
+    {
+        static void Main()
+        {
+            
+        }
+    }
+}
